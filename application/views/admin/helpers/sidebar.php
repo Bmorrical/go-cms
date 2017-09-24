27b83ca1fea27892af0af59d2a3db28854b27f44
Brad Morrical <bradmorricalweb@gmail.com>
2017-09-24 10:28:40 -0500

    fixing admin logo route

--- application/views/admin/helpers/sidebar.php
+++ application/views/admin/helpers/sidebar.php
@@ -16,7 +16,7 @@
 	</div>
 	<div id="logo">
 		<?php if($this->config->item('go_logo_location') != "") : ?>
-			<a href="<?= base_url() . $this->config->item('logo_default_route'); ?>">
+			<a href="<?= $_SERVER['DOCUMENT_ROOT']; ?>">
 				<img class="img-responsive" style="text-align: center;" src="<?php echo base_url() . 'assets/' . $this->config->item('go_logo_location'); ?>">
 			</a>
 		<?php endif; ?>
