21d5b000e8832122b5d98e41a4a8eed5239350e6
Brad Morrical <bradmorricalweb@gmail.com>
2017-10-29 05:21:59 -0500

    adding helper__functions file

--- application/core/GO_Controller.php
+++ application/core/GO_Controller.php
@@ -26,6 +26,7 @@ class GO_Controller extends CI_Controller
 		$this->load->model('go_admin_model','admin');
 
 		require_once(APPPATH . 'go_inc/helpers/helper__functions.php');
+		require_once(APPPATH . 'go_inc/helpers/helper__go_functions.php');
 		require_once(APPPATH . 'go_inc/classes/class__go_login.php');		
 		require_once(APPPATH . 'go_inc/classes/class__go_lab.php');
 		require_once(APPPATH . 'go_inc/classes/class__go_postal.php');		
