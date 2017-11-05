40a772036246fe6ba28466d852c2c594834e19b8
Brad Morrical <bradmorricalweb@gmail.com>
2017-11-04 21:31:54 -0500

    wip

--- application/migrations/1.sql
+++ application/migrations/1.sql
@@ -4,3 +4,7 @@ ADD INDEX `menu_items__MenuItemURL` (`MenuItemURL` ASC);
 
 ALTER TABLE `go_users` 
 ADD INDEX `users__username` (`Username` ASC);
+
+ALTER TABLE `go-cms`.`go_users` 
+CHANGE COLUMN `Username` `Username` VARCHAR(255) CHARACTER SET 'utf8' NOT NULL ,
+ADD UNIQUE INDEX `Username_UNIQUE` (`Username` ASC);
